86572f14bf64c4f83e9a9131dd0f9e4c7a5e887f
Eric Liang <ewliang@users.noreply.github.com>
2018-10-06 17:52:33 -0700

    Update split-strings.js

--- JavaScript/split-strings.js
+++ JavaScript/split-strings.js
@@ -12,3 +12,30 @@
 /*
   MY SOLUTION
 */
+function solution(str){
+   var splitStr = [];
+   let tempString = str.split('');
+   let pairCount = Math.ceil(tempString.length/2);
+   if(tempString.length % 2 == 0) {
+     // Even
+     for(let i = 0; i < pairCount; i++) {
+       // Iterate and push based on pair count.
+       splitStr.push(tempString[i+i] + tempString[i + i + 1]);
+     }
+   } else {
+     // Odd - Append underscore '_' to last character character.
+     for(let i = 0; i < pairCount; i++) {
+       // Iterate and push based on pair count.
+       // Check if current character is the last character.
+       if(i != tempString.length - 1) {
+         // Not the last character
+         if((i + 1) == pairCount) {
+            splitStr.push(tempString[i + i] + '_');
+         } else {
+         	  splitStr.push(tempString[i + i] + tempString[i + i + 1]);
+         }
+       }
+     }
+   }
+   return splitStr;
+}
